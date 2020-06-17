72558d7f5702ad4eda185e0ffa0f50c1d1d2872c
mikuu <biaofu@thoughtworksc.om>
2020-06-17 15:44:39 +0800

    containerize

---
+++ file-server/initializer/initializer.sh
@@ -0,0 +1,6 @@
+mkdir -p /exchange/file-server
+mkdir -p /exchange/file-server/assets
+mkdir -p /exchange/file-server/assets/project-team-image
+mkdir -p /exchange/file-server/projects
+
+http-server -p 8123 /exchange
