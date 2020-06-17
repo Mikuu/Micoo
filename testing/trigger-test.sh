8a78ba79c1329a6f60a5c247a75d772c6e814a32
mikuu <biao.fu@thoughtworks.com>
2020-06-17 23:47:27 +0800

    integrating with nginx

--- testing/trigger-test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "... screenshots are transported to visual compare server."
-curl -X POST "http://localhost:3002/slave/build/initialize?pid=PID14416af374ee4a6db395004b2275eb7f&buildVersion=v43598ghsvloxn728abdr667g"
-
-echo
