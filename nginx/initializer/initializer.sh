04d3785034886e49c4b885d394b82bc046f31ee3
mikuu <biaofu@thoughtworksc.om>
2020-06-18 18:08:10 +0800

    integrating with Nginx

---
+++ nginx/initializer/initializer.sh
@@ -0,0 +1,5 @@
+mkdir -p /exchange/file-server
+mkdir -p /exchange/file-server/assets
+mkdir -p /exchange/file-server/assets/project-team-image
+mkdir -p /exchange/file-server/projects
+nginx -g "daemon off;"
