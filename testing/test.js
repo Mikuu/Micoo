899f90fc58b1b2cde99e956f3566406511bf8bf7
mikuu <biaofu@thoughtworksc.om>
2020-06-19 14:49:33 +0800

    updating compose setup

--- testing/test.js
+++ testing/test.js
@@ -1,9 +1,9 @@
 const { newBuild } = require("../clients/nodejs/micooc");
 
 function test() {
-    const host = "http://localhost:8080/engine";
+    const host = "http://localhost:8123/engine";
     // const host = "http://localhost:3002";
-    const pid = "PIDde319bd86e11426291387552177c036b";
+    const pid = "PID06d09c798df146679ee7f3a0c0f342f4";
     const buildVersion = "5fafc0478af24af2da45fa19ddd06c17dd5d0d45";
     const screenshotDirectory = "./latest";
 
