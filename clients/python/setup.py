5a7b57f11dc17b3522d2ffed9427e81da5e5d564
ariman <biaofu@thoughtworks.com>
2021-02-13 21:45:12 +0800

    update Python client to v0.1.4 to support apiKey

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="micooc",
-    version="0.1.3",
+    version="0.1.4",
     author="ariman",
     author_email="biaofu@thoughtworks.com",
     description="Micoo client for python",
--- testing/python/test.py
+++ testing/python/test.py
@@ -4,26 +4,29 @@ from micooc import new_build, build_stats, latest_build_stats
 def test_new_build():
     service_host = "http://localhost:8123"
     engine_host = service_host + "/engine"
-    pid = "PIDa8e3c0a4444a4f1a90a4dad8bd3467c2"
+    api_key = "AK005fca5cbc9779755f"
+    pid = "PIDc3ac134737084e6596e52b8de1d4be39"
 
     build_version = "5fafc0478af24af2da45fa19ddd06c17dd5d0d45"
     screenshot_directory = "/Users/ariman/Workspace/Expressing/Micoo/testing/latest"
 
-    return new_build(engine_host, pid, build_version, screenshot_directory)
+    return new_build(engine_host, api_key, pid, build_version, screenshot_directory)
 
 
 def test_build_stats():
     service_host = "http://localhost:8123"
-    bid = "BIDfb1c90b110124e10a280d5ac5fc9cd20"
+    api_key = "AK005fca5cbc9779755f"
+    bid = "BID707ac2c98b93419f9081c4c13bd19e07"
 
-    return build_stats(service_host, bid)
+    return build_stats(service_host, api_key, bid)
 
 
 def test_latest_build_stats():
     service_host = "http://localhost:8123"
-    pid = "PIDa8e3c0a4444a4f1a90a4dad8bd3467c2"
+    api_key = "AK005fca5cbc9779755f"
+    pid = "PIDc3ac134737084e6596e52b8de1d4be39"
 
-    return latest_build_stats(service_host, pid)
+    return latest_build_stats(service_host, api_key, pid)
 
 
 if __name__ == "__main__":
