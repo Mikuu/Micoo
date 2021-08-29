6e6ca7943348a5c14f7a9636e701617c8be3726d
ariman <biaofu@thoughtworks.com>
2021-08-29 19:52:44 +0800

    update version, dashboard 0.1.9 -> 0.2.0, engine 0.1.5 -> 0.1.6

--- testing/nodejs/test-public.js
+++ testing/nodejs/test-public.js
@@ -2,13 +2,13 @@ import { newBuild, buildStats, latestBuildStats } from "micooc";
 
 async function testNewBuild() {
     // host for containerized service.
-    // const host = "http://localhost:8123/engine";
+    const host = "http://localhost:8123/engine";
 
      // host for engine service lunched from local source code.
-    const host = "http://localhost:3002";
+    // const host = "http://localhost:3002";
 
-    const apiKey = "AKed8bcb0f3ca75e98a3";
-    const pid = "PID2139551567d24f228180d9170348eeb9";
+    const apiKey = "AK5b2a5d79e25be67b27";
+    const pid = "PID031c82c63f6140c981bcbbe566f9c384";
     const buildVersion = "5fafc0478af24af2da45fa19ddd06c17dd5d0d45";
     const screenshotDirectory = "../latest";
 
@@ -16,19 +16,19 @@ async function testNewBuild() {
 }
 
 async function testBuildStats() {
-   // const host = "http://localhost:8123";
-    const host = "http://localhost:3001";
+   const host = "http://localhost:8123";
+    // const host = "http://localhost:3001";
     const bid = "BIDdb67dcb3231548cbae5574dd40c2e28e";
-    const apiKey = "AKed8bcb0f3ca75e98a3";
+    const apiKey = "AK5b2a5d79e25be67b27";
 
     console.log(await buildStats(host, apiKey, bid));
 }
 
 async function testLatestBuildStats() {
-   // const host = "http://localhost:8123";
-    const host = "http://localhost:3001";
-    const pid = "PID2139551567d24f228180d9170348eeb9";
-    const apiKey = "AKed8bcb0f3ca75e98a3";
+   const host = "http://localhost:8123";
+    // const host = "http://localhost:3001";
+    const pid = "PID031c82c63f6140c981bcbbe566f9c384";
+    const apiKey = "AK5b2a5d79e25be67b27";
 
     console.log(await latestBuildStats(host, apiKey, pid));
 }
