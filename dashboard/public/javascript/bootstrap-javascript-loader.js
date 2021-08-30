082857c870de68d9ebc88854ff46491750ff9807
ariman <biaofu@thoughtworks.com>
2021-08-30 23:13:48 +0800

    update dashboard version 0.2.1 -> 0.2.2, fix some bugs introduced by implementing ignoring.

---
+++ dashboard/public/javascript/bootstrap-javascript-loader.js
@@ -0,0 +1,40 @@
+const checkAndIncludeBootstrapJS = () => {
+	const bootstrapJS = [
+		"/public/javascript/jquery-3.5.1.slim.min.js",
+		"/public/javascript/bootstrap.bundle.min.js"
+	];
+
+	const isScriptAlreadyIncluded = (src) => {
+		let scripts = document.getElementsByTagName("script");
+		for (let i = 0; i < scripts.length; i++) {
+			if (scripts[i].getAttribute('src') === src) {
+				return true;
+			}
+		}
+		return false;
+	};
+
+	const generateJS = (src) => {
+		const script = document.createElement("script");
+		script.src = src;
+		script.crossorigin = "anonymous";
+		const body = document.getElementsByTagName("body")[0];
+		body.appendChild(script);
+	};
+
+	for (const src of bootstrapJS) {
+		if (!isScriptAlreadyIncluded(src)) {
+			generateJS(src);
+			console.log(`no ${src} in document, created it now.`);
+		} else {
+			console.log(`${src} already in document.`);
+		}
+	}
+};
+
+/**
+ * usually the bootstrap JS need be loaded globally at the end of body to enable the boostrap functions, while sometimes
+ * the boostrap JS need be preloaded before executing some customized JS function which interact with boostrap behavior,
+ * so just dynamically load the bootstrap JS.
+ * */
+checkAndIncludeBootstrapJS();
--- testing/nodejs/test-public.js
+++ testing/nodejs/test-public.js
@@ -2,13 +2,13 @@ import { newBuild, buildStats, latestBuildStats } from "micooc";
 
 async function testNewBuild() {
     // host for containerized service.
-    const host = "http://localhost:8123/engine";
+    // const host = "http://localhost:8123/engine";
 
      // host for engine service lunched from local source code.
-    // const host = "http://localhost:3002";
+    const host = "http://localhost:3002";
 
-    const apiKey = "AK5b2a5d79e25be67b27";
-    const pid = "PID031c82c63f6140c981bcbbe566f9c384";
+    const apiKey = "AK9bac8f6a49645b3ef8";
+    const pid = "PID961c369a748c4bdf9fae369bcde4475c";
     const buildVersion = "5fafc0478af24af2da45fa19ddd06c17dd5d0d45";
     const screenshotDirectory = "../latest";
 
@@ -16,19 +16,19 @@ async function testNewBuild() {
 }
 
 async function testBuildStats() {
-   const host = "http://localhost:8123";
-    // const host = "http://localhost:3001";
+   // const host = "http://localhost:8123";
+    const host = "http://localhost:3001";
     const bid = "BIDdb67dcb3231548cbae5574dd40c2e28e";
-    const apiKey = "AK5b2a5d79e25be67b27";
+    const apiKey = "AK9bac8f6a49645b3ef8";
 
     console.log(await buildStats(host, apiKey, bid));
 }
 
 async function testLatestBuildStats() {
-   const host = "http://localhost:8123";
-    // const host = "http://localhost:3001";
-    const pid = "PID031c82c63f6140c981bcbbe566f9c384";
-    const apiKey = "AK5b2a5d79e25be67b27";
+   // const host = "http://localhost:8123";
+    const host = "http://localhost:3001";
+    const pid = "PID961c369a748c4bdf9fae369bcde4475c";
+    const apiKey = "AK9bac8f6a49645b3ef8";
 
     console.log(await latestBuildStats(host, apiKey, pid));
 }
